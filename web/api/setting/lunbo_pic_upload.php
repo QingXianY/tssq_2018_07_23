e5d37bed737f88facebe44f430442051225609c9
hbo <328070823@qq.com>
2018-07-27 15:23:49 +0800

    aaa

--- web/api/setting/lunbo_pic_upload.php
+++ web/api/setting/lunbo_pic_upload.php
@@ -6,6 +6,6 @@ include_once "../../../config/file.php";
 
 $image_file=$_FILES['file'];//得到传输的数据
 $file=new file();
-$result = $file->upload_image_file($image_file,"../../images/lunbo/",time(),'https://wx.xingda188.com/xyn_2018_06_18/web/images/lunbo/');
+$result = $file->upload_image_file($image_file,"../../images/lunbo/",time(),'https://wx.xingda188.com/tssq_2018_07_23/web/images/lunbo/');
 
 echo json_encode($result);
